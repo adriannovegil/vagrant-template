9c9f4c1415198d4ada7065aafa73d6c2706249bf
Novegil Toledo <adrian.novegil@gmail.com>
2017-03-03 10:20:19 +0100

    Updated the network configuration. Prior verion. Now we can add a gateway to the network configuratión. The interfaces configuration is in a separated block.

--- lib/virtualbox/util/network.rb
+++ lib/virtualbox/util/network.rb
@@ -6,19 +6,46 @@ require 'json'
 def configureNetwork(machine_instance, server_config)
   # Iterate over the network configuration
   if server_config.has_key?("network")
-    # Iterate
-    server_config['network'].each do |interface|
-      type = interface['network-type']
-      # We only handle private and public networks
-      next if type != "private" && type != "public"
-      # Configure it
-      if type == "public"
-        configurePublicNetwork(machine_instance, interface)
-      elsif type == "private"
-        configurePrivateNetwork(machine_instance, interface)
+    network_config = server_config["network"]
+    puts JSON.pretty_generate(network_config)
+    # Configure the gateway
+    if network_config.has_key?("gateway")
+      configureGateway(machine_instance, network_config["gateway"])
+    end
+    # Configure interfaces
+    if network_config.has_key?("interfaces")
+      interfaces_config = network_config["interfaces"]
+      # Iterate over the network interfaces
+      interfaces_config.each do |interface|
+        type = interface['network-type']
+        # We only handle private and public networks
+        next if type != "private" && type != "public"
+        # Configure it
+        if type == "public"
+          configurePublicNetwork(machine_instance, interface)
+        elsif type == "private"
+          configurePrivateNetwork(machine_instance, interface)
+        end
       end
     end
   end
+end
+
+def configureGateway(machine_instance, gateway)
+  # default router
+  machine_instance.vm.provision "shell",
+    run: "always",
+    inline: "route add default gw " + gateway
+
+  # default router ipv6
+  #config.vm.provision "shell",
+  #  run: "always",
+  #  inline: "route -A inet6 add default gw fc00::1 eth1"
+
+  # delete default gw on eth0
+  #machine_instance.vm.provision "shell",
+  #  run: "always",
+  #  inline: "eval `route -n | awk '{ if ($8 ==\"eth0\" && $2 != \"0.0.0.0\") print \"route del default gw \" $2; }'`"
 end
 
 #
